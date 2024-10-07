abed2a9f17698f1e76c447f5e4aad0b122aaff62
mateusfigmelo <andyjim815@gmail.com>
2024-10-07 12:36:50 -0500

    add interact_with_abi.rs

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,17 @@ mod conversion;
 mod create_instance;
 mod deploy_from_artifacts;
 mod deploy_from_bytecode;
+mod interact_with_abi;
 mod math_operations;
 mod math_utilities;
 
 fn main() {
-    //comparison::main();
-    //let _ = conversion::main();
-    //let _ = create_instance::main();
-    //let _ = math_operations::main();
-    //let _ = math_utilities::main();
-    //let _ = deploy_from_artifacts::main();
+    comparison::main();
+    let _ = conversion::main();
+    let _ = create_instance::main();
+    let _ = math_operations::main();
+    let _ = math_utilities::main();
+    let _ = deploy_from_artifacts::main();
     let _ = deploy_from_bytecode::main();
+    let _ = interact_with_abi::main();
 }
