a4802cd6d25fdf6618ad2a2c485feb42e7aa2660
mateusfigmelo <andyjim815@gmail.com>
2024-10-07 11:54:41 -0500

    add deployf_from_artifacts.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod comparison;
 mod conversion;
 mod create_instance;
+mod deploy_from_artifacts;
 mod math_operations;
 mod math_utilities;
 
@@ -10,4 +11,5 @@ fn main() {
     let _ = create_instance::main();
     let _ = math_operations::main();
     let _ = math_utilities::main();
+    let _ = deploy_from_artifacts::main();
 }
