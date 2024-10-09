3f9a7e6a84da366cb3778efb5bb17ebc80499cda
mateusfigmelo <andyjim815@gmail.com>
2024-10-09 09:50:17 -0500

    add provider.rs

--- src/provider.rs
+++ src/provider.rs
@@ -82,3 +82,48 @@ pub(crate) mod builtin {
         Ok(())
     }
 }
+
+pub(crate) mod http {
+    use alloy::providers::{Provider, ProviderBuilder};
+    use eyre::Result;
+
+    #[tokio::main]
+    pub async fn main() -> Result<()> {
+        println!("*********testing provider http*********");
+        let rpc_url = "https://eth.merkle.io".parse()?;
+        let provider = ProviderBuilder::new().on_http(rpc_url);
+
+        let latest_block = provider.get_block_number().await?;
+        println!("Latest block number: {}", latest_block);
+
+        Ok(())
+    }
+}
+
+pub(crate) mod ws {
+    use alloy::providers::{Provider, ProviderBuilder, WsConnect};
+    use eyre::Result;
+    use futures_util::StreamExt;
+
+    #[tokio::main]
+    pub async fn main() -> Result<()> {
+        println!("*********testing provider ws*********");
+        let rpc_url = "wss://eth-mainnet.g.alchemy.com/v2/6s8PYp-UrorBla48RX2NcAdtTiE2WmtN";
+        let ws = WsConnect::new(rpc_url);
+        let provider = ProviderBuilder::new().on_ws(ws).await?;
+
+        let sub = provider.subscribe_blocks().await?;
+
+        let mut stream = sub.into_stream().take(4);
+        println!("Awaiting blocks...");
+
+        let handle = tokio::spawn(async move {
+            while let Some(block) = stream.next().await {
+                println!("Latest block number: {}", block.header.number);
+            }
+        });
+
+        handle.await?;
+        Ok(())
+    }
+}
